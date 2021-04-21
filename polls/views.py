54eb001b998cea134041d769c70d0a6135664a75
emma-l-myoosik <emma.li.22@bishops.com>
2021-04-21 11:37:48 -0700

    moved the login button, figuring out how to combine post and comment

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .models import Post, Comment
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
