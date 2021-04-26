3f0edb93b2033a8fad9f50e37ecc24315ab27bfa
emma-l-myoosik <emma.li.22@bishops.com>
2021-04-26 11:36:27 -0700

    created login form & added necessary ifs to index views

--- polls/views.py
+++ polls/views.py
@@ -9,9 +9,30 @@ def index(request):
     """
     first view (home page) - questions: need to get the post to actually show up
     """
+    # checks if any form data has been submitted
+    if request.POST:
+        # checks if the form submitted is the login form by checking for username input
+        if 'inputUsername' in request.POST.keys():
+            # attempt authentication of login info
+            user = authenticate(username=request.POST['inputUsername'], password=request.POST['inputPassword'])
+            if user is not None:
+                # if the user exists, then log in the user
+                login(request, user)
+
+    if request.user.is_authenticated:
+        loggedIn = True
+    else:
+        loggedIn = False
+
     allposts = Post.objects.order_by('-pub_date')
     allcomments = Comment.objects.order_by('-pub_date')
-    context = {"allposts": allposts, "allcomments": allcomments}
+    context = {
+        'allposts': allposts,
+        'allcomments': allcomments,
+        'loggedIn': loggedIn,
+        'user': request.user
+        }
+
     print(context)
     return render(request, 'polls/index.html', context)
 
