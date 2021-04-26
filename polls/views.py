a7a7c013ef7f2e2b72dcf5d1f38605463b27c471
emma-l-myoosik <emma.li.22@bishops.com>
2021-04-26 11:48:57 -0700

    added logout form and if() to views

--- polls/views.py
+++ polls/views.py
@@ -11,21 +11,32 @@ def index(request):
     """
     # checks if any form data has been submitted
     if request.POST:
-        # checks if the form submitted is the login form by checking for username input
+        # checks if the form submitted is the login form (by checking for username input)
         if 'inputUsername' in request.POST.keys():
             # attempt authentication of login info
             user = authenticate(username=request.POST['inputUsername'], password=request.POST['inputPassword'])
             if user is not None:
                 # if the user exists, then log in the user
                 login(request, user)
+            else:
+                # go on to the next check
+                pass
+        # checks if the form submitted is the logout form
+        elif 'logout' in request.POST.keys():
+            # kill the section
+            logout(request)
 
+    # sets loggedIn variable to be used in HTML
     if request.user.is_authenticated:
         loggedIn = True
     else:
         loggedIn = False
 
+    # gets all posts and comments from the respective models
     allposts = Post.objects.order_by('-pub_date')
     allcomments = Comment.objects.order_by('-pub_date')
+
+    # creates json
     context = {
         'allposts': allposts,
         'allcomments': allcomments,
@@ -33,7 +44,6 @@ def index(request):
         'user': request.user
         }
 
-    print(context)
     return render(request, 'polls/index.html', context)
 
 def twit(request):
